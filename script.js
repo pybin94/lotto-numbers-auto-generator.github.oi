ba18afdc37a6ea05778d2bcdbc01c1eb15a10703
pybin94 <73331322+pybin94@users.noreply.github.com>
2022-04-11 15:09:33 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,36 @@
-const checkArea = async () {
-    for( let i = 1; i < 46; i++ ){
-        document.querySelector("#lotteryPaper").innerHTML += `<div id="checkArea" class="check-area">${i}</div>`
-    }
+const init = () => {
+    lottoryBox()
 }
 
-checkArea().then(() => {
+const mkCheckArea = async () => {
+
+    let checkBox;
+    checkBox = ""
+
+    for( let i = 1; i < 46; i++ ){
+        checkBox += `<div id="checkArea" class="check-area">${i}</div>`
+    }
+
+    return document.querySelector("#lotteryPaper").innerHTML = checkBox
+}
+
+const lottoryBox = async () => {
+    
+    await mkCheckArea()
+
     let lotto = []; 
     const checkArea = document.querySelectorAll("#checkArea")
     const lotteryResults = document.querySelector("#lotteryResults")
     const autoSel = document.querySelector("#autoSel")
     const cancel = document.querySelector("#cancel")
 
-    const lottoNumSort() => {   // 숫자 오름차순 정렬https://github.com/pybin94/lotto_numbers_auto_generator.github.io/blob/main/script.js
+    const lottoNumSort = () => {   // 오름차순 정렬
         lotto.sort(function(a,b){
             return a - b;
         });
     }
 
-    const lottoNum() => {
+    const lottoNum = () => {
         for(let i = 0; i < 6; i++){
             let num = Math.floor(Math.random() * 44) + 1;
             
@@ -39,8 +52,6 @@ checkArea().then(() => {
         lottoNumSort()
 
     }
-    
-    // lottoNum()
 
     for( let i = 0; i < checkArea.length; i++){  // 로또 번호 클릭 이벤트
         checkArea[i].addEventListener("click", function(){
@@ -76,35 +87,31 @@ checkArea().then(() => {
 
         })
 
-        autoSel.addEventListener("click", function(){  // 자동선택 버튼
-
-            checkArea[i].classList.remove("active")
-
-        })
-
-        cancel.addEventListener("click", function(){  // 취소 버튼
-
-            checkArea[i].classList.remove("active")
-
+    }
+    const reset = () => {
+        lotto = []
+        checkArea.forEach((item, index) => {
+            checkArea[index].classList.remove("active")
         })
     }
-
-    autoSel.addEventListener("click", function(){  // 자동선택 버튼
-
-        lotto = []; 
+    
+    autoSel.addEventListener("click", () => {  // 자동선택 버튼
+        reset()
         lottoNum()
         lotteryResults.innerHTML = "번호 " + lotto
-
     })
-
-    cancel.addEventListener("click", function(){  // 취소 버튼
-
+    
+    cancel.addEventListener("click", () => {  // 취소 버튼
+        reset()
         lotteryResults.innerHTML = "번호 "
-        lotto = []; 
-
     })
 
     // 로또 결과
     lotteryResults.innerHTML = "번호 "
     
-})
+}
+
+
+
+
+init()
